7f63b0fc004b2dd4149e6eaca400c5c8c3dc6b6a
Shahul1507 <shahul.met@gmail.com>
2024-10-19 10:51:02 +0530

    notes

--- 06-docker/docker.sh
+++ 06-docker/docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-
-cd ..
+sudo y
+sudo y
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl start docker
-exit
 sudo usermod -aG docker ec2-user
 exit
