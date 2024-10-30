08497c436b406941185b509ba85caefa4f2f7dae
Shahul1507 <shahul.met@gmail.com>
2024-10-30 22:20:21 +0530

    my-notes

--- 07-kubernetes/Install.sh
+++ 07-kubernetes/Install.sh
@@ -3,7 +3,7 @@
 #Extend the volume of the server
 df -hT
 lsblk
-growpart /dev/xvda 4  # xvda for t2.micro for t3.micro it is nvme
+growpart /dev/nvme0n1 4  # xvda for t2.micro for t3.micro it is nvme
 lvextend -l +50%FREE /dev/RootVG/rootVol
 lvextend -l +50%FREE /dev/RootVG/varVol
 xfs_growfs /
